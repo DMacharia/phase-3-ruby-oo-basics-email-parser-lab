4aa514fb02a3ae3b9f33a1df036d366b32b6f3d8
Daniel Macharia <danielhuios@gmail.com>
2022-10-22 05:44:06 +0300

    successfully parsed through space and comma delimited email data

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,19 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    attr_accessor :emails
+
+    def initialize(emails)
+        @emails = emails
+    end
+
+    def parse
+        @emails.split(/, | /).uniq.map do |email|
+            email
+        end
+    end
+end
+
+email = EmailAddressParser.new("avi@test.com, arel@test.com test@avi.com, test@arel.com avi@test.com")
+puts email.parse
